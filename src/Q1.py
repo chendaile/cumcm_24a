e88ac34eaa539a8b718eb89f84ae13ec00175664
chendaile <3185854290@qq.com>
2025-09-01 04:39:10 +0800

    update

--- src/Q1.py
+++ src/Q1.py
@@ -1,6 +1,7 @@
 import numpy 
 import pandas
 import matplotlib.pyplot as plt 
+from math import pi, sqrt
 
 class BenchDragonNode:
     def __init__(self, length, index, 
@@ -23,19 +24,31 @@ class BenchDragonNode:
         self.front_enter = False if self.front_linear_pos < 0 else True
         self.back_enter = False if self.back_linear_pos < 0 else True
 
+        self.front_polar_pos = self.get_polar(self.front_linear_pos) if self.front_enter else None
+        self.back_polar_pos = self.get_polar(self.back_linear_pos) if self.back_enter else None
+
+    def get_polar(self, linear_pos, a=16*55, b=55/(pi*2)):
+        """
+        The spiral formula is r=a-b*theta
+        """
+        theta = (a - sqrt(a**2 - 2*linear_pos*b)) / b
+        r = a - b * theta
+
+        return (r, theta)
+
 class BenchDragon:
     def __init__(self, moment):
         """
         Time unit is seconds
         """
         self.NodeSum = 223
-        self.Nodes_lineared = {}
+        self.Nodes = {}
         
         HeadSpeed = 100
         self.Head_linear_pos = HeadSpeed * moment
-        self.Nodes_lineared[1] = BenchDragonNode(341, 1, self.Head_linear_pos)
+        self.Nodes[1] = BenchDragonNode(341, 1, self.Head_linear_pos)
         for i in range(2, self.NodeSum+1):
-            front_linear_pos_i = self.Nodes_lineared[i-1].back_linear_pos
-            self.Nodes_lineared[i] = BenchDragonNode(220, i, front_linear_pos=front_linear_pos_i)
+            front_linear_pos_i = self.Nodes[i-1].back_linear_pos
+            self.Nodes[i] = BenchDragonNode(220, i, front_linear_pos=front_linear_pos_i)
         
     
