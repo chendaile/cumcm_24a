e7157ea475c52c402dd43c591f82a9152736dd7d
chendaile <3185854290@qq.com>
2025-09-01 02:25:16 +0800

    update

---
+++ src/Q1.py
@@ -0,0 +1,41 @@
+import numpy 
+import pandas
+import matplotlib.pyplot as plt 
+
+class BenchDragonNode:
+    def __init__(self, length, index, 
+                 head_linear_pos: float = None, front_linear_pos: float = None, 
+                 width=30, hole_distance=27.5):
+        """
+        All units are centimeters
+        """
+        self.length = length
+        self.index = index
+        self.width = width
+        self.hole_distance = hole_distance
+
+        if front_linear_pos is None:
+            self.front_linear_pos = head_linear_pos - self.hole_distance
+        else:
+            self.front_linear_pos = front_linear_pos
+        self.back_linear_pos = self.front_linear_pos - (self.length - 2*self.hole_distance)
+        
+        self.front_enter = False if self.front_linear_pos < 0 else True
+        self.back_enter = False if self.back_linear_pos < 0 else True
+
+class BenchDragon:
+    def __init__(self, moment):
+        """
+        Time unit is seconds
+        """
+        self.NodeSum = 223
+        self.Nodes_lineared = {}
+        
+        HeadSpeed = 100
+        self.Head_linear_pos = HeadSpeed * moment
+        self.Nodes_lineared[1] = BenchDragonNode(341, 1, self.Head_linear_pos)
+        for i in range(2, self.NodeSum+1):
+            front_linear_pos_i = self.Nodes_lineared[i-1].back_linear_pos
+            self.Nodes_lineared[i] = BenchDragonNode(220, i, front_linear_pos=front_linear_pos_i)
+        
+    
